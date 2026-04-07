6940fd4baf2bb651c033997a7d08eaf2b6629266
YusufFidanci <yusuffidanci2255@gmail.com>
2026-04-07 18:51:38 +0300

    Initial project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace movieArchieve.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
